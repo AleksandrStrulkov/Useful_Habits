6ce761c122e2fc7eba1dde9322e87167dd5328a4
AleksandrStrulkov <astrulkov@yandex.ru>
2024-03-11 16:43:32 +0300

    Change settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-toms!!!-$hiuuag3donlne)32hfz!*=&au4+5u2b&ue(+ja72f"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
